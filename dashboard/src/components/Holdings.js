7d13a2416771433003eafdf2569417556c4b60c8
roshani9324 <roshanimaurya.in@gmail.com>
2026-07-02 23:31:36 +0530

    host add

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -8,10 +8,12 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3002/allHoldings").then((res) => {
-      console.log(res.data);
-      setAllHoldings(res.data);
-    });
+    axios
+      .get("https://zerodha-application-qi06.onrender.com/allHoldings")
+      .then((res) => {
+        console.log(res.data);
+        setAllHoldings(res.data);
+      });
   }, []);
 
   // const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
